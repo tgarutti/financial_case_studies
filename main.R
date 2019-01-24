30591f35aefd9c6e85ec2161cc8b44b5969b8503
Unknown <tgarutti@gmail.com>
2019-01-24 17:21:58 +0100

    its beautiful people

--- main.R
+++ main.R
@@ -6,7 +6,7 @@ source("source_file.R")
 
 ## PCA and analysis of factors
 #Select data needed, either US or UK.
-yieldCurve <- us_quarterly
+yieldCurve <- us_yield_ortec
 countryData <- us_data
 
 #Perform pca on the yield curve data.
@@ -22,16 +22,28 @@ St <- pca$x[,2]
 normSt <- as.zoo(St - mean(St))/sqrt(var(St))
 index(normSt) <- index(yieldCurve)
 
-#Select PCE core inflation from the dataset.
-inflation <- countryData[,5] - mean(countryData[,5])
+#Select PCE core inflation from the dataset. Demean the dataset, using a different mean every x years.
+x <- 100
+inflation <- countryData[,5]
+
+for (i in 1:(floor(length(inflation)/(4*x))+1))
+{
+  s = ((i-1)*4*x+1)
+  e = i*4*x
+  if (e > length(inflation))
+  {
+    inflation[s:length(inflation)] = inflation[s:length(inflation)] - mean(inflation[s:length(inflation)])
+  }
+  else
+  {
+    inflation[s:e] = inflation[s:e] - mean(inflation[s:e])
+  }
+}
+
+inflation <- inflation - mean(inflation)
 
 #Plot the level factor against inflation.
 plot(-normLt/100, ylim = c(-0.02,0.03), col = "green")
-lines(inflation)
-
-normPC1 <- as.zoo((pcOrtec[,1] - mean(pcOrtec[,1])/sqrt(var(pcOrtec[,1]))))
-plot(-normLt/100, ylim = c(-0.02,0.03), col = "green")
-lines(-normPC1/100, col = "red")
 lines(inflation)
 ##------------------------------------------------------------------##
 ##------------------------------------------------------------------##
--- read_data.R
+++ read_data.R
@@ -14,6 +14,9 @@ us_yield_ortec <- read.zoo("./data/us_yield_ortec.csv", header = TRUE, sep = ","
 us_yieldCurve <- interpolate(united_states)[,c(1,2,4,8,10,11,12,14,17)]
 #us_yieldCurve <- interpolate(united_states)[,c(1,2,4,8,10,12)]
 
+us_yield_ortec <- interpolate(us_yield_ortec)[,c(1,2,4,8,10,12,14,17)]
+us_yield_ortec <- window(us_yield_ortec, start = "1985-03-1", end = "2018-03-01")
+
 #uk_yieldCurve <- interpolate(united_kingdom)[,c(1,2,4,8,10,11,12,13,14,15,16,17)]
 uk_yieldCurve <- interpolate(united_kingdom)[,c(1,2,4,8,10,12,13,14,15,16,17)]
 
@@ -25,7 +28,7 @@ uk_quarterly <- as.zoo(monthlyToQuarterly(window(uk_yieldCurve, start = "1995-01
 colnames(uk_quarterly) = colnames(uk_yieldCurve)
 index(uk_quarterly) = index(uk_data)[1:93]
 
-us_data <- window(us_data, start = "1990-03-1", end = "2018-03-01")
+us_data <- window(us_data, start = "1985-03-1", end = "2018-03-01")
 uk_data <- window(uk_data, start = "1995-03-1", end = "2018-03-01")
 
 rm(united_kingdom, united_states)
