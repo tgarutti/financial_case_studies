08f33de609da067080dff895bf33d26fcc5ad18b
Unknown <tgarutti@gmail.com>
2019-02-02 17:53:42 +0100

    Corrected final table of coefficients

--- matlab/main.m
+++ matlab/main.m
@@ -20,6 +20,12 @@ set(gca, 'xtick', [1:132], 'xticklabel', xTickLabels);
 
 
 %% Moving window regressions -> coefficient matrix
+coefficients_shortRate = [];
+coefficients_Lt = [];
+coefficients_St = [];
+coefficients_residuals = [];
+coefficients_inflation = [];
+coefficients_outputGap = [];
 
 for i = 1:(n-w+1)
     window = i:(i+w-1);
@@ -29,12 +35,14 @@ for i = 1:(n-w+1)
     Y_it = 100*shortRate(window(1):window(end));
     
     [coefit, ~, residualit] = lsqlin(X_it, Y_it, [], []);
-    
+    coefit = coefit';
+
     % Regression of Lt
     X_lt = [Lt(window(1):window(end-1)) 100*inflation(window(1):window(end-1))];
     Y_lt = Lt(window(2):window(end));
     
     [coefLt, ~, residualLt] = lsqlin(X_lt, Y_lt, [], [], ones(1,size(X_lt,2)), 1);
+    coefLt = coefLt';
     
     % Regression of St
     X_st = [St(window(1):window(end-1)) 100*outputGap(window(2):window(end))...
@@ -42,13 +50,14 @@ for i = 1:(n-w+1)
     Y_st = St(window(2):window(end));
     
     [coefSt, ~, residualSt] = lsqlin(X_st, Y_st, [], []);
-    coefSt = [coefSt(1), 1-coefSt(1), coefSt(2)/(1-coefSt(1)), coefSt(3)/(1-coefSt(1))];
+    coefSt = [coefSt(1) 1-coefSt(1) coefSt(2)/(1-coefSt(1)) coefSt(3)/(1-coefSt(1))];
     
     % Regression of St regression error ut
     X_res = residualSt(1:(end-1));
     Y_res = residualSt(2:end);
     
     [coefres, ~, residualres] = lsqlin(X_res, Y_res, [], []);
+    coefres = coefres';
     
     % Regression of inflation
     X_pi = [Lt(window(3):window(end)) 100*inflation(window(2):window(end-1))...
@@ -56,8 +65,8 @@ for i = 1:(n-w+1)
     Y_pi = 100*inflation(window(3):window(end));
     
     [coefpi, ~, residualpi] = lsqlin(X_pi, Y_pi, [], []);
-    coefpi = [coefpi(1), 1-coefpi(1), coefpi(2)/(1-coefpi(1)),...
-        coefpi(3)/(1-coefpi(1)), coefpi(4)];
+    coefpi = [coefpi(1) 1-coefpi(1) coefpi(2)/(1-coefpi(1))...
+        coefpi(3)/(1-coefpi(1)) coefpi(4)];
     
     % Regression of output gap
     Eyt = 100*outputGap + normrnd(0,1,size(outputGap));
@@ -66,6 +75,17 @@ for i = 1:(n-w+1)
     Y_y = 100*outputGap(window(3):window(end-1));
     
     [coefy, ~, residualy] = lsqlin(X_y, Y_y, [], []);
-    coefy = [coefy(1), 1-coefy(1), coefy(2)/(1-coefy(1)),...
-        coefy(3)/(1-coefy(1)), coefy(4)];    
+    coefy = [coefy(1) 1-coefy(1) coefy(2)/(1-coefy(1))...
+        coefy(3)/(1-coefy(1)) coefy(4)];
+    
+    % Coefficient matrix
+    coefficients_shortRate = [coefficients_shortRate; coefit];
+    coefficients_Lt = [coefficients_Lt; coefLt];
+    coefficients_St = [coefficients_St; coefSt];
+    coefficients_residuals = [coefficients_residuals; coefres];
+    coefficients_inflation = [coefficients_inflation; coefpi];
+    coefficients_outputGap = [coefficients_outputGap; coefy];
 end
+coefficients = table(coefficients_shortRate, coefficients_Lt,...
+    coefficients_St, coefficients_residuals, coefficients_inflation,...
+    coefficients_outputGap);
