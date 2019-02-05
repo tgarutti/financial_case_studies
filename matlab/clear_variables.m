d892e4b17118831c2aa3182163543298fdc965c5
Unknown <tgarutti@gmail.com>
2019-02-05 16:38:48 +0100

    Corrected clear variables

---
+++ matlab/clear_variables.m
@@ -0,0 +1,11 @@
+%% Clear variables
+clear res_it res_Lt res_pi res_res res_St res_y
+clear coefficients_inflation coefficients_Lt coefficients_outputGap...
+    coefficients_residuals coefficients_shortRate coefficients_St
+clear Aeq beq r p options initial estimates deltaL deltaS zeta Pi Q Sigma...
+    z Theta1 Theta2 C eps j k omegaSims gammaSims
+clear X_it X_lt X_pi X_res X_st X_y
+clear Y_it Y_lt Y_pi Y_res Y_st Y_y
+clear coefit coefLt coefpi coefres coefSt coefy
+clear residualit residualLt residualpi residualres residualSt residualy
+clear Gamma Omega eu
--- matlab/main.m
+++ matlab/main.m
@@ -19,22 +19,12 @@ initialize_variables
 for i = 1:(n-w+1)
     window = i:(i+w-1);
     run_regressions % Runs regressions from a separate file "run_regressions.m"
-    
+    F = 
     macroStateSpace
-    
-    clear X_it X_lt X_pi X_res X_st X_y
-    clear Y_it Y_lt Y_pi Y_res Y_st Y_y
-    clear coefit coefLt coefpi coefres coefSt coefy
-    clear residualit residualLt residualpi residualres residualSt residualy
-    clear Gamma Omega eu
 end
 clear w n i window
 coefficients = table(coefficients_shortRate, coefficients_Lt,...
     coefficients_St, coefficients_residuals, coefficients_inflation,...
     coefficients_outputGap);
 
-clear res_it res_Lt res_pi res_res res_St res_y
-clear coefficients_inflation coefficients_Lt coefficients_outputGap...
-    coefficients_residuals coefficients_shortRate coefficients_St
-clear Aeq beq r p options initial estimates deltaL deltaS zeta Pi Q Sigma...
-    z Theta1 Theta2 C eps j k omegaSims gammaSims
+clear_variables
