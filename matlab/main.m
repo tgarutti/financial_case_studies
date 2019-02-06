3ae27e4b2f2d0e1d0fe68bd7d328a1f21c6de8e6
Themis Rallis <t.rallis@live.com>
2019-02-06 10:18:36 +0100

    .

--- matlab/main.m
+++ matlab/main.m
@@ -5,13 +5,13 @@ load_data
 normPCs = normalize(PCs);
 
 %% Define regression window and variables
-w = 40; % widnow length of 10 years
+w = 40; % Window length of 10 years
 n = length(inflation);
 Lt = normPCs(:,1);
 St = normPCs(:,2);
 
 %% Plots
- %plots
+%plots
 
 %% Moving window regressions and Sims algorithm
 initialize_variables
@@ -22,8 +22,9 @@ for i = 1:(n-w+1)
     run_regressions % Runs regressions from a separate file "run_regressions.m"
     coefficientsB % Obtains the B coefficients for the yield curve
     macroStateSpace % Runs the Kalman filter and performs MLE
-    lastXi(i,:) = xi(:,end)';
+    lastXi(i,:) = xi(:,end)'; % Sets end values of filtered variables
 end
+
 coefficients = table(coefficients_shortRate, coefficients_Lt,...
     coefficients_St, coefficients_residuals, coefficients_inflation,...
     coefficients_outputGap);
