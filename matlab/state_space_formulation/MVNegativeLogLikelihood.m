e1e1c13f53ef18f5ded447f6f6d782d9e0072c78
Themis Rallis <t.rallis@live.com>
2019-02-07 12:34:18 +0100

    Big changes. See description
    - Kalman filter redefined as in macroStateSpace
    - Updated the Kalman filter accordingly
    - Added a 'Kalman gain' factor
    - Updated negative loglikelihood

--- matlab/state_space_formulation/MVNegativeLogLikelihood.m
+++ matlab/state_space_formulation/MVNegativeLogLikelihood.m
@@ -1,21 +1,27 @@
 function [output] = MVNegativeLogLikelihood(parameter_vector,y)
 
-% Set global variable for filtered states
-global predictedxi
+% Call global variables
+global predictedxi H1 H2 z
 
 % Extract length of the data, and the dimensionality of the problem
 T = size(y,1);
-d = size(y,2);
+d = 2; % 2 states, xi_t = [L_t S_t]'
+l = 3; % 3 observations, x_t = [i_t pi_t y_t]'
 
 % Extract what is needed from parameter_vector
-H     = zeros(4,1);
-%H(1)  = parameter_vector(19);
-%H(2)  = parameter_vector(20);
-%sigma = parameter_vector(21);
+S  = zeros(l,l);
+Q  = zeros(l,d);
 
-H(1)  = parameter_vector(10);
-H(3)  = parameter_vector(11);
-sigma = parameter_vector(12);
+Q(1,1) = parameter_vector(7);
+Q(1,2) = parameter_vector(8);
+Q(2,1) = parameter_vector(9);
+Q(2,2) = parameter_vector(10);
+Q(3,1) = parameter_vector(11);
+Q(3,2) = parameter_vector(12);
+
+S(1,1) = parameter_vector(13);
+S(2,2) = parameter_vector(14);
+S(3,3) = parameter_vector(15);
 
 % Run the Kalman filter
 [~,~,predictedxi,predictedP] = MVKalmanFilter(parameter_vector,y);
@@ -27,10 +33,10 @@ LL     = zeros(1,T);
 
 % Collect a row vector of log likelihood per observation
 for t=3:T
-    Sigma(:,:,t) = H'*predictedP(:,:,t)*H+sigma;
-    mu(:,t)      = H'*predictedxi(:,t);
+    Sigma(:,:,t) = Q*predictedP(:,:,t)*Q'+S;
+    mu(:,t)      = Q*predictedxi(:,t)-H1*z(:,t-1)-H2*z(:,t-2);
     LL(t)        = log(1/sqrt(det(2*pi*Sigma(:,:,t)))*...
-                exp(-1/2*(y(t)-mu(:,t))'*(Sigma(:,:,t)\(y(t)-mu(:,t)))));
+                exp(-1/2*(y(:,t)-mu(:,t))'*(Sigma(:,:,t)\(y(:,t)-mu(:,t)))));
 end
 
 % Sum over all observations
