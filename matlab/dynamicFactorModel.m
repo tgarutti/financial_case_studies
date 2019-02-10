0eead9c744842d747f960ab0199898519b496e68
Unknown <tgarutti@gmail.com>
2019-02-10 23:03:30 +0100

    K step ahead forecast

--- matlab/dynamicFactorModel.m
+++ matlab/dynamicFactorModel.m
@@ -9,9 +9,10 @@ w = 80; % Window length of 10 years
 n = length(inflation);
 u = n-w+1; % Number of filters
 X = 100*[inflation outputGap unemployment];
+k = 4; % k-step ahead forecast
 
 %% Forecast the dynamic factor model
-forecasts = zeros(u,size(X,2)+1);
+forecasts = zeros(u,size(X,2)+1, k);
 for i = 1:u
     window = i:(i+w-1);
     
@@ -19,6 +20,8 @@ for i = 1:u
     PCw = PCOrtec(window,:);
     favar = varm(3,2);
     [estFit,~,~,~] = estimate(favar, Xw, 'X', PCw);
-    [f, fMSE] = forecast(estFit, 1, Xw, 'X', PCw);
-    forecasts(i,2:end) = f;
+    [f, fMSE] = forecast(estFit, k, Xw, 'X', PCw);
+    for j = 1:k
+        forecasts(i,2:end,j) = f(j,:);
+    end
 end
