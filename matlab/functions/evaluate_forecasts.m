fddc4ebad448d13f4137cf8db22ab2afb85a842f
Unknown <tgarutti@gmail.com>
2019-02-11 10:17:10 +0100

    Forecast evaluations

---
+++ matlab/functions/evaluate_forecasts.m
@@ -0,0 +1,9 @@
+function [ forecast_evaluations ] = evaluate_forecasts( forecasts, actuals )
+%EVALUATE_FORECASTS Evaluates forecasts: RMSE, MSE
+%   Evaluates the forecasts versus the actual values and calculates
+%   RMSE and MSE for each column of forecasts. Column 1 of forecasts
+%   must therefore correspond to column 1 of actuals.
+
+
+end
+
