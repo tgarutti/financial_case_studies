9338cfff3038321d6eea64fe075663c3c0725ab8
Themis Rallis <t.rallis@live.com>
2019-02-12 11:31:33 +0100

    Added AR restrictions; simsCheck even more robust

--- matlab/state_space_formulation/simsCheck.m
+++ matlab/state_space_formulation/simsCheck.m
@@ -2,7 +2,7 @@ function [target] = simsCheck(sims,tolerance)
 % Function that checks the Sims output
 
 if sims<=tolerance
-    target = (0.1+rand)/5;
+    target = 0.3+(rand/5);
 else
     target = sims;
 end
