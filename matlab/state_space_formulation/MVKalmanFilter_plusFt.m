e2f4668f8d4c0c039bd4062e1b0844e3a7d37fa3
Themis Rallis <t.rallis@live.com>
2019-02-19 13:09:32 +0100

    Created hybrid SSM/DFM, results are insanely good

--- matlab/state_space_formulation/MVKalmanFilter_plusFt.m
+++ matlab/state_space_formulation/MVKalmanFilter_plusFt.m
@@ -1,7 +1,7 @@
 function [xi,P,predictedxi,predictedP] = MVKalmanFilter_plusFt(parameter_vector,y)
 
 % Call global variables
-global z PCOrtec_window
+global z Ft
 
 % Extract length of the data, and the dimensionality of the problem
 T = size(y,2);
@@ -18,6 +18,7 @@ Theta2 = zeros(d,d);
 H1 = zeros(l,d);
 H2 = zeros(l,d);
 H3 = zeros(3,10);
+c = zeros(l,1);
 
 Pi(1,1) = parameter_vector(1);
 Pi(1,2) = parameter_vector(2);
@@ -40,44 +41,47 @@ S(2,3) = parameter_vector(15);
 S(3,2) = parameter_vector(16);
 S(3,3) = parameter_vector(17);
 
-Theta1(1,1) = parameter_vector(18);
-Theta1(1,2) = parameter_vector(19);
-Theta1(2,1) = parameter_vector(20);
-Theta1(2,2) = parameter_vector(21);
+H1(2,1) = parameter_vector(18);
+H1(2,2) = parameter_vector(19);
+H1(3,2) = parameter_vector(20);
 
-Theta2(1,1) = parameter_vector(22);
-Theta2(1,2) = parameter_vector(23);
-Theta2(2,1) = parameter_vector(24);
-Theta2(2,2) = parameter_vector(25);
+H2(2,1) = parameter_vector(21);
+H2(3,2) = parameter_vector(22);
 
-H1(2,1) = parameter_vector(26);
-H1(2,2) = parameter_vector(27);
-H1(3,2) = parameter_vector(28);
+c(1) = parameter_vector(23);
+c(2) = parameter_vector(24);
+c(3) = parameter_vector(25);
 
-H2(2,1) = parameter_vector(29);
-H2(3,2) = parameter_vector(30);
+Theta1(1,1) = parameter_vector(26);
+Theta1(1,2) = parameter_vector(27);
+Theta1(2,1) = parameter_vector(28);
+Theta1(2,2) = parameter_vector(29);
 
-H3(2,1)  = parameter_vector(31);
-H3(2,2)  = parameter_vector(32);
-H3(2,3)  = parameter_vector(33);
-H3(2,4)  = parameter_vector(34);
-H3(2,5)  = parameter_vector(35);
-H3(2,6)  = parameter_vector(36);
-H3(2,7)  = parameter_vector(37);
-H3(2,8)  = parameter_vector(38);
-H3(2,9)  = parameter_vector(39);
-H3(2,10) = parameter_vector(40);
+Theta2(1,1) = parameter_vector(30);
+Theta2(1,2) = parameter_vector(31);
+Theta2(2,1) = parameter_vector(32);
+Theta2(2,2) = parameter_vector(33);
 
-H3(3,1)  = parameter_vector(41);
-H3(3,2)  = parameter_vector(42);
-H3(3,3)  = parameter_vector(43);
-H3(3,4)  = parameter_vector(44);
-H3(3,5)  = parameter_vector(45);
-H3(3,6)  = parameter_vector(46);
-H3(3,7)  = parameter_vector(47);
-H3(3,8)  = parameter_vector(48);
-H3(3,9)  = parameter_vector(49);
-H3(3,10) = parameter_vector(50);
+H3(2,1) = parameter_vector(34);
+H3(2,2) = parameter_vector(35);
+H3(2,3) = parameter_vector(36);
+H3(2,4) = parameter_vector(37);
+H3(2,5) = parameter_vector(38);
+H3(2,6) = parameter_vector(39);
+H3(2,7) = parameter_vector(40);
+H3(2,8) = parameter_vector(41);
+H3(2,9) = parameter_vector(42);
+H3(2,10) = parameter_vector(43);
+H3(3,1) = parameter_vector(44);
+H3(3,2) = parameter_vector(45);
+H3(3,3) = parameter_vector(46);
+H3(3,4) = parameter_vector(47);
+H3(3,5) = parameter_vector(48);
+H3(3,6) = parameter_vector(49);
+H3(3,7) = parameter_vector(50);
+H3(3,8) = parameter_vector(51);
+H3(3,9) = parameter_vector(52);
+H3(3,10) = parameter_vector(53);
 
 % Diffuse initialisation
 mu0    = zeros(d,1);
@@ -106,7 +110,7 @@ for t=3:T
     
     % Updating step
     xi(:,t)  = predictedxi(:,t)+kalmanGain(:,:,t)*...
-        (y(:,t)-Q*predictedxi(:,t)-H1*z(:,t-1)-H2*z(:,t-2)-H3*PCOrtec_window(:,t));
+        (y(:,t)-c-Q*predictedxi(:,t)-H1*z(:,t-1)-H2*z(:,t-2)-H3*Ft(:,t));
     P(:,:,t) = (eye(d)-kalmanGain(:,:,t)*Q)*predictedP(:,:,t);
     % Close the loop over time
 end
--- matlab/state_space_formulation/MVNegativeLogLikelihood_plusFt.m
+++ matlab/state_space_formulation/MVNegativeLogLikelihood_plusFt.m
@@ -1,7 +1,7 @@
 function [output] = MVNegativeLogLikelihood_plusFt(parameter_vector,y)
 
 % Call global variables
-global predictedxi z PCOrtec_window
+global xi z Ft
 
 % Extract length of the data, and the dimensionality of the problem
 T = size(y,2);
@@ -9,7 +9,7 @@ l = size(y,1); % 3 observations, x_t = [i_t pi_t y_t]'
 d = 2; % 2 states, xi_t = [L_t S_t]'
 
 % Run the Kalman filter
-[~,~,predictedxi,predictedP] = MVKalmanFilter_plusFt(parameter_vector,y);
+[xi,~,predictedxi,predictedP] = MVKalmanFilter_plusFt(parameter_vector,y);
 
 % Extract what is needed from parameter_vector
 S  = zeros(l,l);
@@ -17,6 +17,7 @@ Q  = zeros(l,d);
 H1 = zeros(l,d);
 H2 = zeros(l,d);
 H3 = zeros(l,10);
+c = zeros(l,1);
 
 Q(1,1) = parameter_vector(9);
 Q(1,2) = parameter_vector(10);
@@ -29,34 +30,37 @@ S(2,3) = parameter_vector(15);
 S(3,2) = parameter_vector(16);
 S(3,3) = parameter_vector(17);
 
-H1(2,1) = parameter_vector(26);
-H1(2,2) = parameter_vector(27);
-H1(3,2) = parameter_vector(28);
+H1(2,1) = parameter_vector(18);
+H1(2,2) = parameter_vector(19);
+H1(3,2) = parameter_vector(20);
 
-H2(2,1) = parameter_vector(29);
-H2(3,2) = parameter_vector(30);
+H2(2,1) = parameter_vector(21);
+H2(3,2) = parameter_vector(22);
 
-H3(2,1)  = parameter_vector(31);
-H3(2,2)  = parameter_vector(32);
-H3(2,3)  = parameter_vector(33);
-H3(2,4)  = parameter_vector(34);
-H3(2,5)  = parameter_vector(35);
-H3(2,6)  = parameter_vector(36);
-H3(2,7)  = parameter_vector(37);
-H3(2,8)  = parameter_vector(38);
-H3(2,9)  = parameter_vector(39);
-H3(2,10) = parameter_vector(40);
+c(1) = parameter_vector(23);
+c(2) = parameter_vector(24);
+c(3) = parameter_vector(25);
 
-H3(3,1)  = parameter_vector(41);
-H3(3,2)  = parameter_vector(42);
-H3(3,3)  = parameter_vector(43);
-H3(3,4)  = parameter_vector(44);
-H3(3,5)  = parameter_vector(45);
-H3(3,6)  = parameter_vector(46);
-H3(3,7)  = parameter_vector(47);
-H3(3,8)  = parameter_vector(48);
-H3(3,9)  = parameter_vector(49);
-H3(3,10) = parameter_vector(50);
+H3(2,1) = parameter_vector(34);
+H3(2,2) = parameter_vector(35);
+H3(2,3) = parameter_vector(36);
+H3(2,4) = parameter_vector(37);
+H3(2,5) = parameter_vector(38);
+H3(2,6) = parameter_vector(39);
+H3(2,7) = parameter_vector(40);
+H3(2,8) = parameter_vector(41);
+H3(2,9) = parameter_vector(42);
+H3(2,10) = parameter_vector(43);
+H3(3,1) = parameter_vector(44);
+H3(3,2) = parameter_vector(45);
+H3(3,3) = parameter_vector(46);
+H3(3,4) = parameter_vector(47);
+H3(3,5) = parameter_vector(48);
+H3(3,6) = parameter_vector(49);
+H3(3,7) = parameter_vector(50);
+H3(3,8) = parameter_vector(51);
+H3(3,9) = parameter_vector(52);
+H3(3,10) = parameter_vector(53);
 
 % Initialize arrays
 mu     = zeros(l,T);
@@ -66,7 +70,7 @@ LL     = zeros(1,T);
 % Collect a row vector of log likelihood per observation
 for t=3:T
     covar(:,:,t) = Q*(predictedP(:,:,t)*Q')+S;
-    mu(:,t)      = Q*predictedxi(:,t)+H1*z(:,t-1)+H2*z(:,t-2)+H3*PCOrtec_window(:,t);
+    mu(:,t)      = c+Q*predictedxi(:,t)+H1*z(:,t)+H2*z(:,t-1)+H3*Ft(:,t);
     LL(t)        = log(1/sqrt(det(2*pi*covar(:,:,t)))*...
                 exp(-1/2*(y(:,t)-mu(:,t))'*(covar(:,:,t)\(y(:,t)-mu(:,t)))));
 end
